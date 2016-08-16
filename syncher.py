3fb6966088e952bbc6d54ba2b5d1e1ff383fafcd
Jeremy R. Manning <jeremy.r.manning@dartmouth.edu>
2016-08-15 21:00:33 -0400

    WIP (messing with config file)

--- syncher.py
+++ syncher.py
@@ -12,14 +12,37 @@ global CONFIG_FNAME
 global CONFIG_FD
 CONFIG_FNAME = '~/config.json'
 
-#create config file if it doesn't exist
-if not os.path.isfile(CONFIG_FNAME):
-    CONFIG_FD = initialize_config_file()
-else:
-    CONFIG_FD = open(CONFIG_FNAME, 'r+')
 
-def initialize_config_file():
-    userName = 'User'
-    userID = '12345'
-    
+def initialize_withings_config_file():
+    CONFIG_FD = open(CONFIG_FNAME, 'w+')
+
+    config = dict()
+    config['withings'] = dict()
+    config['fitbit'] = dict()
+
+    config['withings']['user_name'] = 'User'
+    config['withings']['user_id'] = '12345'
+
+    config['fitbit']['user_name'] = 'User'
+    config['fitbit']['user_id'] = '12345'
+
+    js.JSONEncoder()
+
+
+
+
+def main():
+    #create config file if it doesn't exist
+    if not os.path.isfile(CONFIG_FNAME):
+        CONFIG_FD = initialize_withings_config_file()
+    else:
+        CONFIG_FD = open(CONFIG_FNAME, 'r+')
+
+
+
+
+
+
+
+
 
